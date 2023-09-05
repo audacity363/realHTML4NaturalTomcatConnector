ed43b74f3f31ab078a04a3f2b01c0b1be7dca9d5
Tom Engemann <tom@engemann.me>
2023-09-05 08:24:37 +0200

    Using Autoconf to generate the Makefile

--- natural-ce_compile.sh
+++ natural-ce_compile.sh
@@ -2,7 +2,7 @@
 
 docker build -t rh4nbuild:9.2.2 -f Dockerfile.build .
 docker container rm rh4nbuild
-docker run -it --rm --entrypoint '' -v./:/build --name rh4nbuild rh4nbuild:9.2.2 bash -c "cd /build; make all"
+docker run -it --rm --entrypoint '' -v./:/build --name rh4nbuild rh4nbuild:9.2.2 bash -c "cd /build; ./configure && make all"
 docker build -t rh4n:9.2.2 .
 docker container rm -f rh4n
-docker run -d --name rh4n -p8083:8080 -p2701:2700 rh4n:9.2.2
+docker run -d --name rh4n -p8083:8080 -p2701:2700 -v./docker/coredumps:/mnt/wslg/dumps/ rh4n:9.2.2
