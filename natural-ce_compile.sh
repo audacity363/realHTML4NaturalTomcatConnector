ee805c5559b54c3b3da14851364ba15a6598d9f0
Tom Engemann <tom@engemann.me>
2023-09-05 11:41:30 +0200

    Changed Docker images to multistaging

--- natural-ce_compile.sh
+++ natural-ce_compile.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+mkdir -p bin realHTML4NaturalCore/bin
 docker build -t rh4nbuild:9.2.2 -f Dockerfile.build .
 docker container rm rh4nbuild
-docker run -it --rm --entrypoint '' -v./:/build --name rh4nbuild rh4nbuild:9.2.2 bash -c "cd /build; ./configure && make all"
+
+docker run -it --name rh4nbuild rh4nbuild:9.2.2
+docker cp rh4nbuild:/build/realHTML4NaturalCore/bin/librealHTML4Natural.so ./realHTML4NaturalCore/bin/librealHTML4Natural.so
+docker cp rh4nbuild:/build/realHTML4NaturalCore/bin/realHTML4NaturalNatCaller ./realHTML4NaturalCore/bin/realHTML4NaturalNatCaller
+docker cp rh4nbuild:/build/bin/librealHTMLconnector.so ./bin/librealHTMLconnector.so
+docker cp rh4nbuild:/build/bin/realHTML4Natural.war ./bin/realHTML4Natural.war
+docker container rm rh4nbuild
+
 docker build -t rh4n:9.2.2 .
 docker container rm -f rh4n
 docker run -d --name rh4n -p8083:8080 -p2701:2700 -v./docker/coredumps:/mnt/wslg/dumps/ rh4n:9.2.2
